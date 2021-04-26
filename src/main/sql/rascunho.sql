ad40aec6c73b405f0b9119aef1b43110db78f7ac
Tiago Matos <ti_matos@id.uff.br>
2021-04-26 17:50:59 -0300

    exercicios

---
+++ src/main/sql/rascunho.sql
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  tiago
+ * Created: 19 de abr. de 2021
+ */
+
+CREATE TABLE S (
+	S varchar,
+	NOME varchar,
+	STATUS integer,
+	CIDADE varchar
+);
